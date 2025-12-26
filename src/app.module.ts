8cc27d087cb3a791afab5dfa160d00574c757be9
MatoxEdriel <ga99_buggi@hotmail.com>
2025-12-25 22:55:32 -0500

    Implementacion de clientes y modulos de cliente de prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+
+
+    PrismaModule
+
+    ,
+    ConfigModule.forRoot({
+      isGlobal: true
+
+
+    }),
+
+
+
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
