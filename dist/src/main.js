0d8fe7c427446f79c9aa886097e9996ffd7e57e5
MatoxEdriel <ga99_buggi@hotmail.com>
2026-01-18 00:55:07 -0500

    Instalacion de Swagger
    -uso de set up api para que redirija al puerto con swagger levantado

--- dist/src/main.js
+++ dist/src/main.js
@@ -6,12 +6,21 @@ const common_1 = require("@nestjs/common");
 const config_1 = require("@nestjs/config");
 const transform_interceptor_1 = require("./core/interceptor/transform.interceptor");
 const http_exception_filter_1 = require("./core/filter/http-exception.filter");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.setGlobalPrefix('api');
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('TechFix Api')
+        .setDescription('Documentacion de la API')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
     const configService = app.get(config_1.ConfigService);
     const port = configService.get('PORT') || 3000;
     const corsOrigin = configService.get('CORS_ORIGIN') || '*';
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
     app.enableCors({
         origin: corsOrigin,
         credentials: true
@@ -23,7 +32,12 @@ async function bootstrap() {
     }));
     app.useGlobalInterceptors(new transform_interceptor_1.TransformInterceptor());
     app.useGlobalFilters(new http_exception_filter_1.AllExceptionsFilter());
+    app.getHttpAdapter().get('/', (req, res) => {
+        res.redirect('/api');
+    });
     await app.listen(port);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    console.log(`Server up by ${port}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,29 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TransformInterceptor } from './core/interceptor/transform.interceptor';
 import { AllExceptionsFilter } from './core/filter/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+
+  const config = new DocumentBuilder()
+    .setTitle('TechFix Api')
+    .setDescription('Documentacion de la API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
 
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>('PORT') || 3000;
 
   const corsOrigin = configService.get<string>('CORS_ORIGIN') || '*';
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document)
 
   app.enableCors({
 
@@ -34,13 +47,24 @@ async function bootstrap() {
   //con esto hacemos que las respuesta que envio tengan un formato 
 
 
-  
+
   app.useGlobalInterceptors(new TransformInterceptor())
 
   app.useGlobalFilters(new AllExceptionsFilter())
 
 
+  app.getHttpAdapter().get('/', (req, res) => {
+    res.redirect('/api');
+  });
+
 
   await app.listen(port);
+
+  SwaggerModule.setup('api', app, document)
+  //dirigir automaticamente
+
+
+  console.log(`Server up by ${port}`)
+
 }
 bootstrap();
